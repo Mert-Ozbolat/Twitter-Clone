b58e2ef27601f18132e78646278e901fe62688f1
Mert-Ozbolat <mertozbolat008@gmail.com>
2024-12-31 13:24:30 +0300

    updated

--- src/pages/login/forgot-password.jsx
+++ src/pages/login/forgot-password.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
 import Modal from '../../components/modal'
 import { sendPasswordResetEmail } from 'firebase/auth'
 import { auth } from '../../firebase'
@@ -7,10 +7,11 @@ import { toast } from 'react-toastify'
 const ForgotPassword = () => {
 
     const [isOpen, setIsOpen] = useState(false)
+    const inputRef = useRef()
 
-    const handleSubmit = (e) => {
+    const handleReset = (e) => {
         e.preventDefault();
-        const email = e.target[0].value
+        const email = inputRef.current.value
         sendPasswordResetEmail(auth, email)
             .then(() => {
                 toast.info(
@@ -30,16 +31,17 @@ const ForgotPassword = () => {
                 onClick={() => setIsOpen(true)}>Şifreni mi unuttun?</span>
 
             <Modal isOpen={isOpen} close={() => setIsOpen(false)}>
-                <form onSubmit={handleSubmit} className='flex flex-col gap-3'>
+                <div className='flex flex-col gap-3'>
                     <h1 className='text-3xl'>Şifreni mi unuttun?</h1>
 
                     <p className='text-gray-400'>Email adresinize bir şifre değiştirme bağlantısı gönderilecek</p>
 
-                    <input type="text" className='mt-5 input' />
+                    <input type="text" className='mt-5 input' ref={inputRef} />
 
-                    <button type='submit' className='bg-white hover:bg-gray-300 transition text-black rounded-full mt-8 py-1'>Mail Gönder</button>
-                    <button type='button' className='bg-white hover:bg-gray-300 transition text-black rounded-full mt-4 py-1'>iptal</button>
-                </form>
+                    <button onClick={handleReset} className='bg-white hover:bg-gray-300 transition text-black rounded-full mt-8 py-1'>Mail Gönder</button>
+
+                    <button className='bg-white hover:bg-gray-300 transition text-black rounded-full mt-4 py-1'>iptal</button>
+                </div>
             </Modal>
         </>
     )
